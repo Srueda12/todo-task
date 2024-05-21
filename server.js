f07428e14088a02b2ab68c39b5af8cfb482e07fb
Srueda12 <158604875+Srueda12@users.noreply.github.com>
2024-05-20 22:58:33 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const api = require('./api');
+const port = process.env.PORT || 3000;
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.listen(port, function () {
+    console.log("Server is listening at port: " + port);
+});
+
+app.get('/', function (req, res) {
+    res.send("hello word");
+});
+app.use('/api', api);
